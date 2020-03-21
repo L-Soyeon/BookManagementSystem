88053941fa4a474bf560c6bf89d46708e2729247
L-Soyeon <82108@LAPTOP-HFADEBO0>
2020-03-22 01:26:19 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("*** Student Management System Menu ***");
+		System.out.println("1. Add Book");
+		System.out.println("2. Delete Book");
+		System.out.println("3. Edit Book");
+		System.out.println("4. View Book");
+		System.out.println("4. Exit");
+		System.out.println("Select one number between 1 - 5 : ");
+		System.out.println("_______________________________________");
+	}
+}
