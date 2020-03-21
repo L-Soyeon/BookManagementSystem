0c85a3d04fc2907245aef48e6d20f7f55713bbfe
L-Soyeon <82108@LAPTOP-HFADEBO0>
2020-03-22 01:38:52 +0900

    menu/add some info

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class MenuManager {
 	public static void main (String[] args) {
 		System.out.println("*** Student Management System Menu ***");
 		System.out.println("1. Add Book");
